25d20303f9ecea1482dfe788fa0fe9cec6b53ab2
Sahilsingh1998 <singhsahil.8340@gmail.com>
2022-10-27 01:08:44 +0530

    form

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let flag = 1;
+
+
+function validatefrom() {
+    if (username.value == "") {
+        document.getElementById("userError").innerHTML = "User Name is Empty";
+        flag = 0;
+    } else if (username.value.length < 3) {
+        document.getElementById("userError").innerHTML = "User name Require Minimum 3 char";
+        flag = 0;
+    } else {
+        document.getElementById("userError").innerHTML = "";
+        flag = 1;
+    }
+
+if (password.value == "") {
+    document.getElementById("passError").innerHTML = "Password Is empty";
+    flag = 0;
+}else {
+        document.getElementById("passError").innerHTML = "";
+        flag = 1;
+    }
+    if (flag) {
+        return true;
+    } else (flag){
+        return false;
+    }
+
+
+    
+
+}
+
+
